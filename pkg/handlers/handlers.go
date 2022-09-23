628266f4c57c20e4556a2891f6dedea8f6c2e63b
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-23 15:35:08 +0100

    commit

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,21 +1,46 @@
 package handlers
 
-func GetUser() {
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"net/http"
+)
 
+var ErrowMethodNotAllowed = "method not allowed"
+
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
 }
 
-func CreateUser() {
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetchUser(email, tableName, dynaClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusOK, result)
 
+	}
+
+	result, err := user.FetchUsers(tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 }
 
-func UpdateUser() {
-
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
 }
 
-func DeleteUser() {
-
+func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
 }
 
+func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+}
 func UnhandledMethod() {
 
 }
