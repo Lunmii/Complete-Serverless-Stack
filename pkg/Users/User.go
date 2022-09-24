2649873146eff71fd318cf96edbeb2131c1c4924
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-24 07:14:38 +0100

    commit

--- pkg/Users/User.go
+++ pkg/Users/User.go
@@ -40,3 +40,18 @@ func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*
 	}
 	return item, nil
 }
+
+func FetchUsers(tableName string, dynaClient dynamodbiface.DynamoDBAPI)(*[]User, error){
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	result, err := dynaClient.Scan(input)
+	if err != nil{
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	item := new([]User)
+	err = dynamodbattribute.UnmarshalMap(result.Items, item)
+	return item, nil
+}
